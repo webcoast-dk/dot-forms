857ccf95a6267d2bc818b297b2b3aef3b5badf3d
Thorben Nissen <thorben@webcoast.dk>
2025-02-06 10:57:31 +0100

    [RELEASE] 1.0.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Dot Forms',
     'description' => 'Create virtual fields just like flex forms, but with pure TCA.',
-    'version' => '1.0.4',
+    'version' => '1.0.5',
     'category' => 'be',
     'constraints' => [
         'depends' => [
