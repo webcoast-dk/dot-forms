296484b1a182545ea9debe24c4bee89feddfb60c
Thorben Nissen <thorben@webcoast.dk>
2025-04-28 12:03:51 +0200

    [FEATURE] Refactor DotFormsDataProvider
    * use TCA schema factory to only process fields, necessary for this record type
    * ensure, the field is set, to avoid undefined array key errors

--- Classes/Form/FormDataProvider/DotFormsDataProvider.php
+++ Classes/Form/FormDataProvider/DotFormsDataProvider.php
@@ -7,40 +7,50 @@ namespace WEBcoast\DotForms\Form\FormDataProvider;
 
 
 use TYPO3\CMS\Backend\Form\FormDataProviderInterface;
+use TYPO3\CMS\Core\Schema\TcaSchemaFactory;
 
 class DotFormsDataProvider implements FormDataProviderInterface
 {
+    public function __construct(protected readonly TcaSchemaFactory $schemaFactory) {}
 
     public function addData(array $result): array
     {
-        foreach ($GLOBALS['TCA'][$result['tableName']]['columns'] as $fieldName => $fieldConfig) {
-            if (str_contains($fieldName, '.')) {
-                $mainFieldName = substr($fieldName, 0, strpos($fieldName, '.'));
-                if (isset($result['databaseRow'][$mainFieldName])) {
-                    // Get value by dot notation
-                    $fieldParts = explode('.', substr($fieldName, strpos($fieldName, '.') + 1));
-                    $currentArray = json_decode($result['databaseRow'][$mainFieldName], true);
-                    foreach ($fieldParts as $fieldPart) {
-                        if (!is_array($currentArray)) {
-                            $currentArray = [];
-                        }
-                        $currentArray = $currentArray[$fieldPart] ?? null;
+        $schema = $this->schemaFactory->get($result['tableName']);
+        if ($typeField = $schema->getSubSchemaDivisorField()) {
+            $schema = $schema->getSubSchema($result['databaseRow'][$typeField->getName()]);
+        }
+        // Check
+        foreach ($schema->getFields(fn ($field) => str_contains($field->getName(), '.')) as $field) {
+            $fieldName = $field->getName();
+            $mainFieldName = substr($fieldName, 0, strpos($fieldName, '.'));
+            if (isset($result['databaseRow'][$mainFieldName])) {
+                // Get value by dot notation
+                $fieldParts = explode('.', substr($fieldName, strpos($fieldName, '.') + 1));
+                $currentArray = json_decode($result['databaseRow'][$mainFieldName], true);
+                foreach ($fieldParts as $fieldPart) {
+                    if (!is_array($currentArray)) {
+                        $currentArray = [];
                     }
-                    $result['databaseRow'][$fieldName] = $currentArray ?? $result['databaseRow'][$fieldName] ?? null;
+                    $currentArray = $currentArray[$fieldPart] ?? null;
                 }
+                $result['databaseRow'][$fieldName] = $currentArray ?? $result['databaseRow'][$fieldName] ?? null;
+            } else {
+                $result['databaseRow'][$fieldName] = null;
+            }
 
-                if (isset($result['defaultLanguageRow'][$mainFieldName])) {
-                    // Get value by dot notation
-                    $fieldParts = explode('.', substr($fieldName, strpos($fieldName, '.') + 1));
-                    $currentArray = json_decode($result['defaultLanguageRow'][$mainFieldName], true);
-                    foreach ($fieldParts as $fieldPart) {
-                        if (!is_array($currentArray)) {
-                            $currentArray = [];
-                        }
-                        $currentArray = $currentArray[$fieldPart] ?? null;
+            if (isset($result['defaultLanguageRow'][$mainFieldName])) {
+                // Get value by dot notation
+                $fieldParts = explode('.', substr($fieldName, strpos($fieldName, '.') + 1));
+                $currentArray = json_decode($result['defaultLanguageRow'][$mainFieldName], true);
+                foreach ($fieldParts as $fieldPart) {
+                    if (!is_array($currentArray)) {
+                        $currentArray = [];
                     }
-                    $result['defaultLanguageRow'][$fieldName] = $currentArray ?? $result['defaultLanguageRow'][$fieldName] ?? null;
+                    $currentArray = $currentArray[$fieldPart] ?? null;
                 }
+                $result['defaultLanguageRow'][$fieldName] = $currentArray ?? $result['defaultLanguageRow'][$fieldName] ?? null;
+            } else {
+                $result['defaultLanguageRow'][$fieldName] = null;
             }
         }
 
